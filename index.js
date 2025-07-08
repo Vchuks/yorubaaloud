cf9084dd76427983612dda168de83443ffe55b78
PrinceFregene <fregene590@gmail.com>
2025-07-08 18:54:47 +0100

    update

--- index.js
+++ index.js
@@ -209,7 +209,7 @@ function loginUser(event){
 
 // DASHBOARD PAGE
 
-                        //  NAVBAR SLIDE
+                        //  NAVBAR SLIDE at 900px below
 
 const slideBar = document.getElementById("dashLeftS");
 const menuBtn = document.getElementById("menu2");
@@ -226,7 +226,40 @@ menuBtn2.addEventListener("click", menuSlideOut);
 
 function menuSlideOut(){
     slideBar.classList.remove("slide");
+
 }
+
+
+                       //  NAVBAR SLIDE at before 900px
+const navTx = document.getElementsByClassName("navTx2");
+const hText = document.getElementById("hTextt");
+const slideSide = document.getElementById("dashRightS");
+const menuBtn3 = document.getElementById("menu1");
+const menuBtn4 = document.getElementById("menu11");
+
+menuBtn3.addEventListener("click", halfNavBar);
+
+function halfNavBar(){
+      
+    slideBar.classList.add("half1");
+    slideSide.classList.add("half2");
+    navTx.classList.add("half3");
+    hText.classList.add("half4");
+    menuBtn4.classList.add("half5");
+}
+
+menuBtn4.addEventListener("click", fullNavBar);
+
+function fullNavBar(){
+      
+    slideBar.classList.remove("half1");
+    slideSide.classList.remove("half2");
+    navTx.classList.remove("half3");
+    hText.classList.remove("half4");
+    menuBtn4.classList.remove("half5");
+}
+
+
 
 
                       // BOXES
@@ -278,9 +311,9 @@ function boxFunction(){
         }else{
             mainBox.innerHTML += 
                         `<div class="boxes shadow-[1px_10px_30px_-20px_black] rounded-md">
-                            <div class="nv22D rounded-lg">
+                            <button type="button" onclick="modalPopIn()"  class="nv22Btn rounded-lg">
                                 <h6  class="navText nv22 text-base text-white font-normal">${items.title}</h6>
-                            </div>
+                            </button>
                          </div>`                     
         }
 })
@@ -316,7 +349,7 @@ const docDetails = {
 
 const fetchData = await fetch(`${baseUrl}admin/admin_dashboardapi`, docDetails)
 const result3 = await fetchData.json()
-
+              
        const getId1 = document.getElementById("1");
        getId1.innerText = result3.total_number_of_categories;
 
@@ -337,6 +370,7 @@ const result3 = await fetchData.json()
 }
 getDashboardDetails()
 
+
 // logout from dashboard function
 
 function logoutUser(){
@@ -346,3 +380,205 @@ function logoutUser(){
    window.location.href = "login.html";
    }, 1000);
 }
+
+
+
+
+                            // Table
+
+const tableItems = [
+    {  iD: 1,
+       name: "Name",
+       email: "Email",
+       phoneNumber: 0,
+       position: 0,
+       totalScore: 0
+    },
+    {  iD: 2,
+       name: "Name",
+       email: "Email",
+       phoneNumber: 0,
+       position: 0,
+       totalScore: 0
+    },
+    {  iD: 3,
+       name: "Name",
+       email: "Email",
+       phoneNumber: 0,
+       position: 0,
+       totalScore: 0
+    }
+]
+
+                                                     // Top three students TABLE loop 
+                                                     
+                                                     function tableFunc(){
+                                                     
+                                                           const mainTable = document.getElementsByClassName("dash3rdSec1")[0];
+                                                           tableItems.forEach((items)=>{
+                                                     
+                                                             if(items.hasOwnProperty("name")){
+                                                                 mainTable.innerHTML +=
+                                                                                        `<tbody>
+                                                                                         <tr>
+                                                                                             <td id=tdName  class="tbTags tbTagName font-normal text-center text-sm text-black">${items.name}</td>
+                                                                                             <td id="tdEmail"  class="tbTags tbTagEmail font-normal text-center text-sm text-black">${items.email}</td>
+                                                                                             <td id="tdPh"  class="tbTags tbTagPn font-normal text-center text-sm text-black">${items.phoneNumber}</td>
+                                                                                             <td id="tdPos"  class="tbTags tbTagPos font-normal text-center text-sm text-black">${items.position}</td>
+                                                                                             <td id="tdTs"  class="tbTags tbTagTs font-normal text-center text-sm text-black">${items.totalScore}</td>
+                                                                                         </tr>
+                                                                                         </tbody>`
+                                                             }
+                                                           })
+                                                     }
+                                                     tableFunc()
+
+// function for top 3 students TABLE
+
+function top3Students(){
+
+    const getMyHeaders2 = new Headers();
+    getMyHeaders2.append("Authorization", `Bearer ${getToken("theResult")}`);
+
+    const docDetails2 = {
+        method: "GET",
+        headers: getMyHeaders2
+    }
+
+    fetch(`${baseUrl}admin/top_three_students`, docDetails2)
+    .then((response)=> response.json())
+    .then((result)=> { 
+
+        const tagName = document.getElementsByClassName("tbTagName");
+        tagName[0].innerText = result[0].name;
+        tagName[1].innerText = result[1].name;
+        tagName[2].innerText = result[2].name;
+
+        const tagEmail = document.getElementsByClassName("tbTagEmail");
+        tagEmail[0].innerText = result[0].email;
+        tagEmail[1].innerText = result[1].email;
+        tagEmail[2].innerText = result[2].email;
+
+        const tagPn = document.getElementsByClassName("tbTagPn");
+        tagPn[0].innerText = result[0].phone_number;
+        tagPn[1].innerText = result[1].phone_number;
+        tagPn[2].innerText = result[2].phone_number;
+
+        const tagPos = document.getElementsByClassName("tbTagPos");
+        tagPos[0].innerText = result[0].position;
+        tagPos[1].innerText = result[1].position;
+        tagPos[2].innerText = result[2].position;
+
+        const tagTs = document.getElementsByClassName("tbTagTs");
+        tagTs[0].innerText = result[0].total_score;
+        tagTs[1].innerText = result[1].total_score;
+        tagTs[2].innerText = result[2].total_score;
+
+})
+    .catch((error)=> console.error(error))
+}
+
+top3Students() 
+
+                                                 /* Top three students MODAL loop (tapping from the 
+                                                     TABLE-ITEMS(tableItems) array of Object above )*/
+
+                                                 function modalFunc(){
+
+                                                    const modalContainer = document.getElementsByClassName("modalContainer")[0];
+                                                    tableItems.forEach(({name, email, phoneNumber, position, totalScore})=>{
+
+                                                        modalContainer.innerHTML += 
+                                                                                    `<section class="modalContainer2">
+                                                                                         <div class="modalInfoBox rounded-md shadow-[1px_10px_30px_-10px_black]">
+
+                                                                                             <div class="infoTd flex justify-between">
+                                                                                                <h6 class="infoT1 font-normal text-sm ">Name:</h6>
+                                                                                                <h6 class="infoT2 infoName font-normal text-sm ">${`${name}`}</h6>
+                                                                                             </div>
+                                                                                             <div class="infoTd flex justify-between">
+                                                                                                <h6 class="infoT1 font-normal text-sm">Email:</h6>
+                                                                                                <h6 class="infoT2 infoEmail font-normal text-sm ">${`${email}`}</h6>
+                                                                                             </div>
+                                                                                             <div class=" infoTd flex justify-between">
+                                                                                                <h6 class="infoT1 font-normal text-sm">Phone:</h6>
+                                                                                                <h6 class="infoT2 infoPn font-normal text-sm ">${`${phoneNumber}`}</h6>
+                                                                                             </div>
+                                                                                             <div class=" infoTd flex justify-between">
+                                                                                                <h6 class="infoT1 font-normal text-sm">Position:</h6>
+                                                                                                <h6 class="infoT2 infoPos font-normal text-sm ">${`${position}`}</h6>
+                                                                                             </div>
+                                                                                             <div class=" infoTd flex justify-between">
+                                                                                                <h6 class="infoT1 font-normal text-sm">Score:</h6>
+                                                                                                <h6 class="infoT2 infoTs font-normal text-sm ">${`${totalScore}`}</h6>
+                                                                                             </div> 
+
+                                                                                         </div>
+                                                                                    </section>`
+                                                    })
+
+                                                 }
+                                                 modalFunc()
+
+                                                 // Call the pop-up MODAL
+
+                                                 function modalPopIn(){
+                                                    const modalContainer = document.getElementsByClassName("modalContainer")[0];
+                                                    modalContainer.style.display = "block"
+                                                 }
+
+                                                 // Remove the pop-up MODAL
+
+                                                 function modalPopOut(){
+                                                    const modalContainer = document.getElementsByClassName("modalContainer")[0];
+                                                    modalContainer.style.display = "none"
+                                                 }
+
+
+                                                 // Function for top three students MODAL
+
+                                                 function top3StudentsModal(){
+
+                                                    const getMyHeaders3 = new Headers();
+                                                    getMyHeaders3.append("Authorization", `Bearer ${getToken("theResult")}`);
+                                                
+                                                    const docDetails3 = {
+                                                        method: "GET",
+                                                        headers: getMyHeaders3
+                                                    }
+                                                
+                                                    fetch(`${baseUrl}admin/top_three_students`, docDetails3)
+                                                    .then((response)=> response.json())
+                                                    .then((result)=> { 
+
+                                                        const name = document.getElementsByClassName("infoName");
+                                                        name[0].innerHTML = result[0].name
+                                                        name[1].innerHTML = result[1].name
+                                                        name[2].innerHTML = result[2].name
+
+                                                        const email = document.getElementsByClassName("infoEmail");
+                                                        email[0].innerHTML = result[0].email
+                                                        email[1].innerHTML = result[1].email
+                                                        email[2].innerHTML = result[2].email
+
+                                                        const phone = document.getElementsByClassName("infoPn");
+                                                        phone[0].innerHTML = result[0].phone_number
+                                                        phone[1].innerHTML = result[1].phone_number
+                                                        phone[2].innerHTML = result[2].phone_number
+
+                                                        const position = document.getElementsByClassName("infoPos");
+                                                        position[0].innerHTML = result[0].position
+                                                        position[1].innerHTML = result[1].position
+                                                        position[2].innerHTML = result[2].position
+
+                                                        const totalS = document.getElementsByClassName("infoTs");
+                                                        totalS[0].innerHTML = result[0].total_score
+                                                        totalS[1].innerHTML = result[1].total_score
+                                                        totalS[2].innerHTML = result[2].total_score
+                                                
+                                                        
+                                                })
+                                                    .catch((error)=> console.error(error))
+                                                }
+                                                
+                                                top3StudentsModal() 
